5ebaae058d68b26010ef364f6d5beb3fe0f65b18
Kiran Yesley <kyesley60@stuy.edu>
2026-08-12 15:53:07 -0700

    lala

--- main.py
+++ main.py
@@ -11,29 +11,32 @@ E = 3 # Young's modulus (GPa) 50-300
 a = 0.01 # side length of the beam perpendicular to the force (m) 0.005-0.02
 b = 0.01 # side length of the beam parallel to the force (m) 0.005-0.02
 
-I = (a * b^3)/ 12 # bending inertia (m^4)
+I = (a * b **3)/ 12 # bending inertia (m^4)
 
-alpha = (F * L^2)/ (2 * E * I) #non-dimensional load parameter
+alpha = (F * L ** 2)/ (2 * E * I) #non-dimensional load parameter
 
 
 #Setting up functions:
-def solve_phi0(alpha):
+def function(phi0):
     f = lambda phi: 1.0 / np.sqrt(np.sin(phi0) - np.sin(phi))
-    integral = lambda phi0: quad(f, 0, phi0)[0]
-    solve = lambda p: integral(p) - 2*np.sqrt(alpha)
-    return brentq(solve, 0, np.pi/2)
+    return quad(f, 0, phi0)[0]
+
+def solve_phi0(alpha):
+    return brentq(lambda p: function(p) - 2*np.sqrt(alpha), 1e-9, np.pi/2 - 1e-9)
 
 phi0 = solve_phi0(alpha)
 
 def x(phi):
     constant = np.sqrt((2* E * I)/F)
-    return constant * (np.sqrt(np.sin(phi0)) - np.sqrt(np.sin(phi0) - np.sin(phi)))
+    val = constant * (np.sqrt(np.sin(phi0)) - np.sqrt(np.sin(phi0) - np.sin(phi)))
+    return L - val
 
 def y(phi):
     constant = np.sqrt((E*I)/(2*F))
     f = lambda theta: (np.sin(theta)/ np.sqrt(np.sin(phi0)- np.sin(theta)))
-    return constant * integrate.quad(f,0,phi)
+    val = constant * integrate.quad(f,0,phi)[0]
+    return 0 -val 
 
 print(f'x max: {x(phi0)} y max: {y(phi0)}')
 
-for f in range F
+#for f in range F
