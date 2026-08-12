b877627441cede6d7cfb7b24031c31ae8dc77162
Kiran Yesley <kyesley60@stuy.edu>
2026-08-12 15:19:47 -0700

    hahah

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import numpy as np
 from scipy.integrate import quad
 from scipy.optimize import brentq
 #-----------------CONSTANTS-----------------------
-L = 100 # length of the beam (meters)
+L = 100 # length of the beam 
 #-----------------INPUTS---------------------------
-F = 5 # downwards force at tip of beam (newtons)
-E = 3 # modulus of elasticity (n/m^2)
-a = 2 # side length of the beam perpendicular to the force (meters)
-b = 2 # side length of the beam parallel to the force (meters)
+F = 5 # lateral force 
+E = 3 # modulus of elasticity 
+a = 2 # side length of the beam penpendicular to the force
+b = 2 # other side length of the beam parrallel to the force
 
-I = (a * b^3)/ 12 # second moment of area for square cross-section (m^4)
+I = (a * b^3)/ 12
 
-alpha = (F * L^2)/ (2 * E * I) #non-dimensional load parameter
+alpha = (F * L^2)/ (2 * E * I)
 
 def solve_phi0(alpha):
     f = lambda phi: 1.0 / np.sqrt(np.sin(phi0) - np.sin(phi))
     I = lambda phi0: quad(f, 0, phi0)[0]
-    x = lambda p: I(p) - 2*np.sqrt(alpha)
-    return brentq(x, 0, np.pi/2)
+    solve = lambda p: I(p) - 2*np.sqrt(alpha)
+    return brentq(solve, 0, np.pi/2)
 
+def x(phi0)
