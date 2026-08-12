8e238d7d14a9ad4231812d4b01db951617762708
Kiran Yesley <kyesley60@stuy.edu>
2026-08-12 15:14:54 -0700

    lalal

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ I = (a * b^3)/ 12
 
 alpha = (F * L^2)/ (2 * E * I)
 
+def solve_phi0(alpha):
+    f = lambda phi: 1.0 / np.sqrt(np.sin(phi0) - np.sin(phi))
+    I = lambda phi0: quad(f, 0, phi0)[0]
+    x = lambda p: I(p) - 2*np.sqrt(alpha)
+    return brentq(x, 0, np.pi/2)
+
