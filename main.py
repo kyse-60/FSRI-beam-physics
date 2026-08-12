02b75edc88d996d63717a3d18bad9e41bc218d0a
unknown <xzhang10@caltech.edu>
2026-08-12 15:35:23 -0700

    b

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import numpy as np
 from scipy.integrate import quad
 from scipy.optimize import brentq
 #-----------------CONSTANTS-----------------------
-L = 100 # length of the beam 
+L = 1 # length of the beam (m)
 #-----------------INPUTS---------------------------
-F = 1000 # lateral force 
-E = 3 # modulus of elasticity 
-a = 1 # side length of the beam penpendicular to the force
-b = 1 # other side length of the beam parrallel to the force
+F = 100 # downwards force (N) 1-100
+E = 3 # Young's modulus (GPa) 50-300
+a = 0.01 # side length of the beam perpendicular to the force (m) 0.005-0.02
+b = 0.01 # side length of the beam parallel to the force (m) 0.005-0.02
 
-I = (a * b^3)/ 12
+I = (a * b^3)/ 12 # bending inertia (m^4)
 
-alpha = (F * L^2)/ (2 * E * I)
+alpha = (F * L^2)/ (2 * E * I) #non-dimensional load parameter
 
 def solve_phi0(alpha):
     f = lambda phi: 1.0 / np.sqrt(np.sin(phi0) - np.sin(phi))
