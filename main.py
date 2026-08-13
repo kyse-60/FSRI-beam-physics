def935b83127489f2010db085ecccfe1e31911c4
Kiran Yesley <kyesley60@stuy.edu>
2026-08-12 21:52:10 -0700

    lalal

--- main.py
+++ main.py
@@ -4,22 +4,27 @@ from scipy.integrate import quad
 from scipy.optimize import brentq
 import scipy.integrate as integrate
 import pandas as pd
+import warnings
+warnings.filterwarnings('ignore', category=RuntimeWarning)
 #-----------------CONSTANTS-----------------------
 L = 1 # length of the beam (m)
 #-----------------INPUTS---------------------------
-F = 100 # downwards force (N) 10-100
-E = 100 * 10**9 # Young's modulus (GPa) 100-300
-a = 0.005 # side length of the beam perpendicular to the force (m) 0.005-0.02
-b = 0.005 # side length of the beam parallel to the force (m) 0.005-0.02
+F = [10,100] # downwards force (N) 10-100
+E = [100 * 10**9, 300 * 10**9] # Young's modulus (GPa) 100-300
+A = [0.005,0.02] # side length of the beam perpendicular to the force (m) 0.005-0.02
+B = [0.005,0.02] # side length of the beam parallel to the force (m) 0.005-0.02
 
-I = (a * b **3)/ 12 # bending inertia (m^4)
+# I = (a * b **3)/ 12 # bending inertia (m^4)
 
-alpha = (F * L ** 2)/ (2 * E * I) #non-dimensional load parameter
-print(alpha)
+def inertia(a,b):
+    return (a * b **3)/ 12
 
-# def alpha(F,E,a,b):
-#     I = (a * b **3)/ 12 # bending inertia (m^4)
-#     return (F * L ** 2)/ (2 * E * I) #non-dimensional load parameter
+# alpha = (F * L ** 2)/ (2 * E * I) #non-dimensional load parameter
+# print(alpha)
+
+def alpha(F,E,a,b):
+    I = (a * b **3)/ 12 # bending inertia (m^4)
+    return (F * L ** 2)/ (2 * E * I) #non-dimensional load parameter
 
 #Setting up functions:
 def function(phi0):
@@ -29,33 +34,45 @@ def function(phi0):
 def solve_phi0(alpha):
     return brentq(lambda p: function(p) - 2*np.sqrt(alpha), 1e-9, np.pi/2 - 1e-9)
 
-phi0 = solve_phi0(alpha)
-print(phi0)
+# phi0 = solve_phi0(alpha)
+# print(phi0)
 
-def x(phi):
-    constant = np.sqrt((2* E * I)/F)
+def x(phi,e,i,f):
+    constant = np.sqrt((2* e * i)/f)
     val = constant * (np.sqrt(np.sin(phi0)) - np.sqrt(np.sin(phi0) - np.sin(phi)))
     return L - val
 
-def y(phi):
-    constant = np.sqrt((E*I)/(2*F))
+def y(phi,e,i,g):
+    constant = np.sqrt((e*i)/(2*g))
     f = lambda theta: (np.sin(theta)/ np.sqrt(np.sin(phi0)- np.sin(theta)))
     val = constant * integrate.quad(f,0,phi)[0]
     return 0 -val 
 
-print(f'x max: {x(phi0)} y max: {y(phi0)}')
+# print(f'x max: {x(phi0)} y max: {y(phi0)}')
+num = 10
 df = pd.DataFrame()
-for f in range(F):
+for f in np.arange(F[0], F[1], (F[1]-F[0])/num):
     s = pd.Series()
     s.loc['F'] = f
-    for e in range(E):
+    for e in np.arange(E[0], E[1], (E[1]-E[0])/num):
         s.loc['E'] = e
-        for a in range(A):
+        for a in np.arange(A[0], A[1], (A[1]-A[0])/num):
             s.loc['width'] = a
-            for b in range(B):
+            for b in np.arange(B[0], B[1], (B[1]-B[0])/num):
                 s.loc['length'] = a
-                alpha = alpha(f,e,a,b)
-                0(alpha)
+                alph = alpha(f,e,a,b)
+                I = inertia(a,b)
+                phi0 = solve_phi0(alph)
                 phipart = phi0/100 
-                phival =0
+                phival = 2 * (phi0/100)
+                index = 0
                 while phival <= phi0:
+                    coord = (x(phival,e,I,f), y(phival,e,I,f))
+                    s.loc['coord' + str(index)] = coord
+                    phival += phipart
+                    index += 1
+                df = pd.concat([df, s.to_frame().T])
+
+print(df)
+df.to_csv('outputagain.csv', index= False)
+                
