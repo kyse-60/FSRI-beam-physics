a75f5e64802e93246e46bb8c7d80689d5a751637
Kiran Yesley <kyesley60@stuy.edu>
2026-08-18 15:26:53 -0700

    small push

--- visualization.py
+++ visualization.py
@@ -2,19 +2,24 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import ast
 
-FILE = "outputValpha(100)-time-1.8745821999909822s.csv"
-type = "alpha" #regular, alpha, 4eq
-FIGNAME = "trial2.jpg"
+# FILE = "outputValpha(100)-time-1.8745821999909822s.csv"
+# FILE = "output4eq-time-19.17769340000814s.csv"
+FILE = "output4eq-time-28.227802200010046s.csv"
+# FILE = "outputV1.1-time-18.980601800023578s.csv"
+type = "4eq" #regular, alpha, 4eq
+FIGNAME = "trial4eq.1.jpg"
 SAMPLES = 4
-
-#print(df.columns.tolist())
 
 def makeplot(filename, figname,samples):
     df = pd.read_csv(filename)
-    coord_cols = [f"coord{i}" for i in range(11)]
+    if type =="regular" or type == "alpha":
+        num = 11
+    else:
+        num = 10
+    coord_cols = [f"coord{i}" for i in range(num)]
     sample = df.sample(n=samples)
     fig, ax = plt.subplots(figsize=(8, 6))
-    ax.set_aspect("equal")
+    #ax.set_aspect("equal")
     for index, row in sample.iterrows():
         points = [ast.literal_eval(row[c]) for c in coord_cols]
         xs, ys = zip(*points)
@@ -23,7 +28,7 @@ def makeplot(filename, figname,samples):
         if type == "alpha":
             ax.plot(xs, ys, marker="o", label=f"alpha={row['alpha']:.3g}")
         if type == "4eq":
-            return 
+            ax.plot(xs, ys, marker="o", label=f"F={row['F']:.3g}, E={row['E']:.3g}, base={row['base']:.3g}, height={row['height']:.3g}, L={row['length']:.3g}")
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.legend()
